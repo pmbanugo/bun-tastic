52891b34cf116475345d123d2042307ddb385b5a
Peter Mbanugo <p.mbanugo@yahoo.com>
2025-01-14 11:52:35 +0100

    feat: add cache control header

--- index.ts
+++ index.ts
@@ -33,12 +33,37 @@ const server = Bun.serve({
 
 console.log(`Listening on http://localhost:${server.port} ...`);
 
+const WEEK_IN_SECONDS = 7 * 24 * 60 * 60; // 7 days
+const DAY_IN_SECONDS = 86400; // 24 hours
+
 async function getHeaders(file: S3File): Promise<HeadersInit> {
   const stat = await file.stat();
+  const cacheControl = getCacheControl(stat.type);
+
   return {
-    "Content-Type": file.type,
+    "Content-Type": stat.type,
     "Content-Length": stat.size.toString(),
     Etag: stat.etag,
     "Last-Modified": stat.lastModified.toUTCString(),
+    "Cache-Control": cacheControl,
   };
+}
+
+function getCacheControl(contentType: string): string {
+  if (contentType === "text/html") {
+    return "public, max-age=0";
+  }
+
+  if (
+    contentType.match(
+      /^(image|video|audio|font|application\/font-|application\/x-font-|text\/css|application\/javascript)/
+    )
+  ) {
+    return `public, max-age=${WEEK_IN_SECONDS}, stale-while-revalidate=${DAY_IN_SECONDS}`;
+  }
+
+  /* A typical static site content, there aren't many file types
+   * that would fall into this category - we could remove this default and explicitly handle all expected content types above
+   */
+  return "public, max-age=0, must-revalidate";
 }
